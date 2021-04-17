9e6a97bafd812cf618dd0416566141801ef10fd7
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 14:26:42 -0400

    Added description

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+// Name : Angel E Hernandez
+// Date : April 17
+// CIS 1202.800
+// Project name : Templates
+
+#include <string>
+# include<iostream>
+using namespace std;
+
+// define a function half, accepts a single argument and return 
+// value of the same type
